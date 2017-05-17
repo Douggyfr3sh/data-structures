3f80de28129e277bdfe6dfe35b1e2843fafd1660
Douglas Lyford <Douggyfr3sh@gmail.com>
2017-05-17 19:23:28 -0400

    Completed functional shared pattern for stack.

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,19 +3,28 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-  var keys = Object.keys(storage);
+  var counter = 0;
   // Implement the methods below
+  var retVal;
+
   someInstance.push = function(value) {
-    storage[0] = value;
+    storage[counter] = value;
+    counter++;
   };
 
   someInstance.pop = function() {
 
-    delete storage[keys[keys.length-1]];
+    if (counter > 0) {
+      retVal = storage[counter - 1];
+      delete storage[counter - 1];
+      counter--;
+    }
+
+    return retVal;
   };
 
   someInstance.size = function() {
-
+    return counter;
   };
 
   return someInstance;
