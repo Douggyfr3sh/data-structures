f5e94a00a7780b9c1952512a43381dbeaf3bee14
Douglas Lyford <Douggyfr3sh@gmail.com>
2017-05-17 18:22:11 -0400

    Added push and pop methods for functional class pattern creation of stack.

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,19 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var keys = Object.keys(storage);
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[0] = value;
   };
 
   someInstance.pop = function() {
+
+    delete storage[keys[keys.length-1]];
   };
 
   someInstance.size = function() {
+
   };
 
   return someInstance;
