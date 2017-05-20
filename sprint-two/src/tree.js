26c36210d687a704ff326358bd7d644e4b3bf901
Douglas Lyford <Douggyfr3sh@gmail.com>
2017-05-19 22:18:36 -0400

    Implemented Tree in functional shared instantiation pattern.

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,10 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
+
+  newTree.addChild = treeMethods.addChild;
+  newTree.contains = treeMethods.contains;
 
   return newTree;
 };
@@ -11,13 +14,37 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
-};
 
+  var retVal = false;
+  //define an inner recursive function
+  var checkChild = function(node) {
+
+    if (node.value === target) {
+      retVal = true;
+    }
+
+    if (node.children.length > 0) {
+      // iterate through children
+      for (var i = 0; i < node.children.length; i++) {
+        //recursive call for each child
+        checkChild(node.children[i]);
+      }
+    }
+  };
+
+  checkChild(this);
+
+  return retVal;
+};
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+//addChild : O(1) - if you have the index
+//contains : O(n)
